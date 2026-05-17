8775ecc674b269b0b877cbccd610d136e9034e88
faldunero <119874524+faldunero@users.noreply.github.com>
2026-05-16 23:26:44 -0400

    feat: Agrega PSM al backend

--- backend/src/services/ocr.service.js
+++ backend/src/services/ocr.service.js
@@ -2,8 +2,8 @@ const Tesseract = require('tesseract.js');
 
 exports.recognize = async (imagePath) => {
   const result = await Tesseract.recognize(imagePath, 'eng', {
-    tessedit_char_whitelist: 'ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789<'
+    tessedit_char_whitelist: 'ABCDEFGHIJKLM NO PQRSTUVWXYZ0123456789<>\n .',
+    tessedit_pageseg_mode: 6  // SINGLE_BLOCK
   });
-
   return result.data && result.data.text ? result.data.text : '';
 };
